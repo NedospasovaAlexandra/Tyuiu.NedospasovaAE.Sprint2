51031e6029f390846037bf858ca91faa996c304a
Tyuiu.NedospasovaAE@outlook.com <anedospasova34@gmail.com>
2024-05-16 23:31:40 +0500

    Task2

---
+++ Tyuiu.NedospasovaAE.Sprint2.Task2.V23.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.NedospasovaAE.Sprint2.Task2.V23.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.NedospasovaAE.Sprint2.Task2.V23.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e3f0d870-736f-4918-9104-7c18f0179bf6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.NedospasovaAE.Sprint2.Task2.V23/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.NedospasovaAE.Sprint2.Task2.V23.Lib;
+
+namespace Tyuiu.NedospasovaAE.Sprint2.Task2.V23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнила: Недоспасова.А.Э | ИИПб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Оператор if - полная и короткая форма записи                      *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #23                                                             *");
+            Console.WriteLine("* Выполнила: Недоспасова Александра Эдуардовна | ИИПб-23-2                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Тема: Написать программу, которая запрашивает целые значения с          *");
+            Console.WriteLine("* клавиатуры  и вычисляет находится ли точка с координатами X,Y в         *");
+            Console.WriteLine("* заштрихованной области                                                  *");
+            Console.WriteLine("* _______________                                                         *");
+            Console.WriteLine("* _______________                                                         *");
+            Console.WriteLine("* __+++___++++___                                                         *");
+            Console.WriteLine("* __+++___++++___                                                         *");
+            Console.WriteLine("* __++++++++++___                                                         *");
+            Console.WriteLine("* ______+++++++__                                                         *");
+            Console.WriteLine("* ______+++++++__                                                         *");
+            Console.WriteLine("* ______+++++++__                                                         *");
+            Console.WriteLine("* _____+++++++___                                                         *");
+            Console.WriteLine("* _____+++++++___                                                         *");
+            Console.WriteLine("* __+++++++++++__                                                         *");
+            Console.WriteLine("* _______++++_++_                                                         *");
+            Console.WriteLine("* _______________                                                         *");
+            Console.WriteLine("* _______________                                                         *");
+            Console.WriteLine("* _______________                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int x, y;
+            Console.WriteLine("Введите значение переменной x:");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение переменной y:");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            var res = ds.CheckDotInShadedArea(x, y);
+            if (res)
+                Console.WriteLine("Точка находится в заштрихованной области");
+            else
+                Console.WriteLine("Точка не находится в заштрихованной области");
+            Console.ReadKey();
+        }
+    }
+}
